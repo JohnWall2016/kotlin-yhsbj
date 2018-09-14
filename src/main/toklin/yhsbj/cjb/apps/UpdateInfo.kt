e457c47964dad293b12d2608d6e05c290fcf9ed6
JohnWall2016 <5672618@qq.com>
2018-09-14 16:30:27 +0800

    update

--- src/main/toklin/yhsbj/cjb/apps/UpdateInfo.kt
+++ src/main/toklin/yhsbj/cjb/apps/UpdateInfo.kt
@@ -2,7 +2,6 @@ package yhsbj.cjb.apps
 
 import yhsbj.cjb.hncjb.Grinfo
 import yhsbj.cjb.hncjb.GrinfoQuery
-import yhsbj.cjb.hncjb.Result
 import yhsbj.cjb.hncjb.Session
 import yhsbj.util.Excels
 import yhsbj.util.Utils
@@ -10,26 +9,25 @@ import yhsbj.util.save
 
 fun main(args: Array<String>) {
     val fn = "D:\\待遇认证\\2018年\\乡镇街上报认证汇总表\\汇总表2.xls"
-    val wb = Excels.load(fn)
-    val sheet = wb.getSheetAt(0)
+    val wb = Excels.load(fn).use {
+        val sheet = it.getSheetAt(0)
+        Session.user002 {
+            for (i in 4..sheet.lastRowNum) {
+                val row = sheet.getRow(i)
+                val idcard = row.getCell(5).stringCellValue
 
-    Session.user002 {
-        for (i in 4..sheet.lastRowNum) {
-            val row = sheet.getRow(i)
-            val idcard = row.getCell(5).stringCellValue
+                it.send(GrinfoQuery(idcard))
+                val rs = it.get<Grinfo>()
 
-            it.send(GrinfoQuery(idcard))
-            val rs = it.get<Grinfo>()
+                var state = "未在我区参加居保"
+                if (rs.datas.size > 0) {
+                    state = rs.datas[0].jbztCN
+                }
+                (row.getCell(9) ?: row.createCell(9)).setCellValue(state)
 
-            var state = "未在我区参加居保"
-            if (rs.datas.size > 0) {
-                state = rs.datas[0].jbztCN
+                println("$idcard: $state")
             }
-            (row.getCell(9) ?: row.createCell(9)).setCellValue(state)
-
-            println("$idcard: $state")
         }
+        it.save(Utils.appendToFileName(fn, ".new"))
     }
-
-    wb.save(Utils.appendToFileName(fn, ".new"))
 }
